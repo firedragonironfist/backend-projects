8448a891bd9481a33fcae43cd7c73af51171bfbc
Yousuf <firedragonironfist998@gmail.com>
2023-07-25 19:38:42 +0530

    Done with CRUD

--- PSQLCLI/index.js
+++ PSQLCLI/index.js
@@ -1,11 +1,6 @@
-const {program} = require('commander');
+#!/usr/bin/env node
 const {Pool} = require('pg');
-const Cursor = require('pg-cursor');
-/*
-const {Client} = require('pg'); 
-const connectionString = 'postgres://admin:password@localhost:5432/auth';
-const client = new Client({connectionString});
- */
+const { program } = require('commander');
 
 const pool = new Pool({
   user: 'admin',
@@ -15,130 +10,68 @@ const pool = new Pool({
   port: 5432,
 });
 
-/* client.connect()
-  .then(()=> console.log('connected to the database'))
-  .catch(err => console.error('Error connecting to the database:',err.stack)); */
-
 pool.on('error', (err,client)=>{
   console.error('Error:', err);
 });
 
-const query = `
-SELECT *
-FROM test.todos
-`;
-
-(async () => {
-  const client = await pool.connect();
-  const query = 'SELECT * FROM users';
-
-  const cursor = await client.query(new Cursor(query));
-
-  cursor.read(1, (err, rows) => {
-      console.log('We got the first row set');
-      console.log(rows);
-
-      cursor.read(1, (err, rows) => {
-          console.log('This is the next row set');
-          console.log(rows);
-      });
+//READ operation
+program
+  .command('read')
+  .description('Read data from the database')
+  .action(async () => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test.todos');
+      console.log(result.rows);
+      client.release();
+    } catch (err) {
+      console.error('Error executing query:', err);
+    }
   });
-})();
 
-//Inserting values
+// CREATE operation
+program
+  .command('create <id> <description> <status>')
+  .description('Create a new todo in the database')
+  .action(async (id, description, status) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('INSERT INTO test.todos (id, description, status) VALUES ($1, $2, $3)', [id, description, status]);
+      console.log('New todo created successfully!');
+      client.release();
+    } catch (err) {
+      console.error('Error executing query:', err);
+    }
+  });
 
-/* const insert = `
-INSERT INTO test.todos(id, description, status)
-VALUES (3, 'fakfa', true)
-`; 
+// UPDATE operation
+program
+  .command('update <id> <description> <status>')
+  .description('Update an existing todo in the database')
+  .action(async (id, description, status) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('UPDATE test.todos SET description = $1, status = $2 WHERE id = $3', [description, status, id]);
+      console.log('Todo updated successfully!');
+      client.release();
+    } catch (err) {
+      console.error('Error executing query:', err);
+    }
+  });
 
-/* client.query(insert, (err,res)=>{
-  if(err){
-    console.log(err);
-    return;
-  }
-  console.log('Data Inserted successfully');
-  client.end();
-}); 
-*/
+// DELETE operation
+program
+  .command('delete <id>')
+  .description('Delete a todo from the database')
+  .action(async (id) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('DELETE FROM test.todos WHERE id = $1', [id]);
+      console.log('Todo deleted successfully!');
+      client.release();
+    } catch (err) {
+      console.error('Error executing query:', err);
+    }
+  });
 
-
-// Selecting values
-
-/* const select = `
-SELECT *
-FROM test.todos
-`;
-
-client.query(select, (err,res)=>{
-  if(err){
-    console.error(err);
-    return;
-  }
-  for(let row of res.rows){
-    console.log(row);
-  }
-  client.end();
-});
- */
-
-// Specific selection
-
-/* const query = `
-SELECT *
-FROM test.todos
-WHERE id<2
-`;
-
-client.query(query, (err, res) => {
-  if (err) {
-      console.error(err);
-      return;
-  }
-  for (let row of res.rows) {
-      console.log(row);
-  }
-  client.end();
-}); */
-
-
-//Specific updation
-
-/* const query = `
-UPDATE test.todos
-SET status = false
-WHERE description = 'Finish the report'
-`;
-
-client.query(query, (err, res) => {
-  if (err) {
-      console.error(err);
-      return;
-  }
-  if (err) {
-      console.error(err);
-      return;
-  }
-  console.log('Data update successful');
-  client.end();
-}); */
-
-// Delete specific row
-
-/* const query = `
-DELETE FROM test.todos
-WHERE id = 3
-`;
-
-client.query(query, (err, res) => {
-  if (err) {
-      console.error(err);
-      return;
-  }
-  if (err) {
-      console.error(err);
-      return;
-  }
-  console.log('Data delete successful');
-  client.end();
-}); */
+program.parse(process.argv);
