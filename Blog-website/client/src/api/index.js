606827f2c00ab969591e02470a0d9a49e67af4e9
Yousuf <firedragonironfist998@gmail.com>
2023-08-29 19:13:51 +0530

    learned a lesson that git is destructive

--- Blog-website/client/src/api/index.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const url = 'http://localhost:5000/posts';
-
-export const fetchBlogs = () => axios.get(url);
-export const createBlog = (newBlog) => axios.post(url, newBlog);
